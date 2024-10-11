9256a2fe2ce1e2869f980b358005354f9a807cff
Nahin Noor <noorn@wit.edu>
2024-10-11 00:25:55 -0400

    Correctly formatted

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -23,11 +23,11 @@ public class PA2b {
 
 		if (discriminant < 0) {
 			System.out.println("Roots: imaginary");
-			
+
 		} else if (discriminant == 0) {
 			double root = -b / (2 * a);
 			System.out.printf("Root: %.2f%n", root);
-			
+
 		} else {
 			double sqrtDiscriminant = Math.sqrt(discriminant);
 			double root1 = (-b - sqrtDiscriminant) / (2 * a);
